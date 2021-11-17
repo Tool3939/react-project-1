f57c8501878c73948c714a27045d64c120bc472f
Tool3939 <talseg2004@gmail.com>
2021-11-17 22:42:51 +0200

    tipranks project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import Stock from "./Stock";
+import React, { useState, useEffect } from "react";
+import style from "./App.css";
+
+const baseUrl =
+  "https://trautocomplete.azurewebsites.net/api/Autocomplete/GetAutocomplete?name=";
 
 function App() {
+  const [stocks, setStocks] = useState([]);
+  const [input, setInput] = useState("");
+  const [url, setUrl] = useState(baseUrl);
+  const fetchStocks = async () => {
+    try {
+      const response = await fetch(url);
+      const stocks = await response.json();
+      stocks = setStocks(
+        stocks
+          .filter((stock) => stock.category === "ticker")
+          .sort((a, b) =>
+            a.label.toLowerCase().localeCompare(b.label.toLowerCase())
+          )
+          .slice(0, 9)
+      );
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  useEffect(() => {
+    fetchStocks();
+  }, [url]);
+  console.log(stocks);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <input
+        type="text"
+        value={input}
+        onChange={(e) => {
+          setInput(e.target.value);
+          setUrl(`${baseUrl}${e.target.value}`);
+        }}
+      />
+      <div className="grid-container">
+        {stocks.map((stock, index) => {
+          return <Stock key={index} {...stock} index={index} />;
+        })}
+      </div>
+    </main>
   );
 }
 
